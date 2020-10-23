c251d250732879ac2d65eff579c593ab01db462a
Jonathan Pyrak <bobcatpyrak@gmail.com>
2020-10-23 14:13:12 -0400

    gui experimenting

--- FutureValueFrame2/src/FutureValueFrame.java
+++ FutureValueFrame2/src/FutureValueFrame.java
@@ -21,30 +21,51 @@ public class FutureValueFrame extends JFrame
 		}
 		
 		setTitle("Future Value Calculator Friend");
-		setSize(400, 100);
+		setSize(800, 800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationByPlatform(true);
 
 		JPanel panel = new JPanel();
 		add(panel);
+		panel.setLayout(new GridBagLayout());
 		
-		panel.add(new JLabel("Monthly Investment:"));
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.gridx = 0; c.gridy = 0;
+		panel.add(new JLabel("Monthly Investment:"), c);
+		
+		c.gridx = 1; c.gridy = 0;
 		JTextField investmentField = new JTextField(20);
-		panel.add(investmentField);
-		panel.add(new JLabel("Yearly Interest Rate:"));
-		panel.add(new JLabel("Years:"));
-		JTextField yearsField = new JTextField(10);
-		panel.add(yearsField);
-		panel.add(new JLabel("Future Value:"));
+		panel.add(investmentField, c);
 		
+		c.gridx = 0; c.gridy = 1;
+		panel.add(new JLabel("Yearly Interest Rate:"), c);
+		
+		c.gridx = 1; c.gridy = 1;
+		JTextField interestRateField = new JTextField(20);
+		panel.add(interestRateField, c);
+		
+		c.gridx = 0; c.gridy = 2;
+		panel.add(new JLabel("Years:"), c);
+		
+		c.gridx = 1; c.gridy = 2;
+		JTextField yearsField = new JTextField(20);
+		panel.add(yearsField, c);
+		
+		c.gridx = 0; c.gridy = 3;
+		panel.add(new JLabel("Future Value:"), c);
+		
+		c.gridx = 0; c.gridy = 4;
 		JButton calculateButton = new JButton("Calculate");
-		panel.add(calculateButton);
+		panel.add(calculateButton, c);
+		
+		c.gridx = 1; c.gridy = 4;
 		JButton exitButton = new JButton("Exit");
 		exitButton.addActionListener(e -> {System.exit(0);});
 
-		panel.add(exitButton);
+		panel.add(exitButton, c);
 		
-		panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		
 		
 		setVisible(true);
 	}

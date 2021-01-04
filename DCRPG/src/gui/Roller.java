091a81fcf07b0694395bfb81fddd6cceffd84b7d
Jonathan Pyrak <bobcatpyrak@gmail.com>
2021-01-03 20:37:40 -0500

    0.74 roller began

---
+++ DCRPG/src/gui/Roller.java
@@ -0,0 +1,28 @@
+package gui;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class Roller extends JPanel {
+
+	public Roller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Roller(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Roller(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Roller(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
